b8f5dfefd49f57ecff54a581571199952cf17335
J <juanchito29@users.noreply.github.com>
2020-11-12 18:32:55 -0700

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,30 +6,30 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
+  firstName: "Juan",
   middleName: "",
-  lastName: "Shoaib",
+  lastName: "Fernandez",
   message: " Passionate about changing the world with technology. ",
   icons: [
     {
       image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
+      url: "https://github.com/juanchito29",
     },
     {
       image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
+      url: "https://www.facebook.com/juan.j.fernandez.735",
     },
     {
       image: "fa-instagram",
-      url: "https://www.instagram.com/hashirshoaeb/",
+      url: "https://www.instagram.com/jjfa80/",
     },
     {
       image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/hashirshoaeb/",
+      url: "https://www.linkedin.com/in/juan-fernandez-7555254b/",
     },
     {
       image: "fa-twitter",
-      url: "https://www.twitter.com/hashirshoaeb/",
+      url: "https://www.twitter.com/jjfa1980",
     },
   ],
 };
@@ -51,11 +51,11 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  imageLink: require("../editable-stuff/Headshot.jpg"),
   imageSize: 375,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
-  resume: require("../editable-stuff/resume.pdf"),
+    "My name is Juan Fernandez. I’m a Petroleum Engineer professional with 12 years of experience in the Oil & Gas industry. I'm a self-driven and motivated leader highly experienced in the area of physics-based modeling and data analytics. I'm most passionate about bringing people and technology together to drive value within an organization. In my free time I enjoy spending time with my family, competing on machine learning challenges and writing on my personal blog.",
+  resume: require("../editable-stuff/resume_2014.pdf"),
 };
 
 // PROJECTS SECTION
@@ -64,7 +64,7 @@ const about = {
 // If you want to display specfic projects, add the repository names,
 //      i.e ["repository-1", "repo-2"]
 const repos = {
-  show: true,
+  show: false,
   heading: "Recent Projects",
   gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
   reposLength: 4,
@@ -100,14 +100,14 @@ const skills = {
   show: true,
   heading: "Skills",
   hardSkills: [
-    { name: "Python", value: 90 },
-    { name: "SQL", value: 75 },
-    { name: "Data Structures", value: 85 },
-    { name: "C/C++", value: 65 },
-    { name: "JavaScript", value: 90 },
-    { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
+    { name: "Reservoir Simulation", value: 80 },
+    { name: "Rate Transient Analysis", value: 80 },
+    { name: "Pressure Transient Analysis", value: 80 },
+    { name: "IPM/DOF: PetEx suite", value: 70 },
+    { name: "Production Surveillance", value: 95 },
+    { name: "Economics: Aries & PEEP", value: 60 },
+    { name: "Python: scikit-learn, pandas, Numpy, statsmodels, TensorFlow, keras", value: 65 },
+    { name: "Data Storytelling: matplotlib, seaborn, spotfire, tableau & powerBI", value: 90 },
   ],
   softSkills: [
     { name: "Goal-Oriented", value: 80 },
@@ -126,8 +126,8 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
-  email: "hashirshoaeb@gmail.com",
+    "I'm looking forward to networking with like-minded individuals to exchange ideas and explore opportunities for collaboration. If you have any questions or if you just want to say hi, please feel free to email me at",
+  email: "juanchito29@gmail.com",
 };
 
 // Blog SECTION
