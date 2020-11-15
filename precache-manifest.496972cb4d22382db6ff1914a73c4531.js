4b1e02e7c83093a8d9b3d1a128bd4a7a66ec718f
Juan Fernandez <JuanF@Marias-iMac.local>
2020-11-15 16:00:33 -0700

    Updates

---
+++ precache-manifest.496972cb4d22382db6ff1914a73c4531.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b525ae0fee7b1e4573c104b822a8ec81",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "e46dca14d3caddc7ce66",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "668244105d1303686236",
+    "url": "/home/static/js/2.0890e750.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.0890e750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46dca14d3caddc7ce66",
+    "url": "/home/static/js/main.8fb87486.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "d29a73a1a6368215bf57e95c1e402956",
+    "url": "/home/static/media/Headshot.d29a73a1.jpg"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "052c7aecc18bba3766a2a34e7842d12e",
+    "url": "/home/static/media/resume_2014.052c7aec.pdf"
+  }
+]);
